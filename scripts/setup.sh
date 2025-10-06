38a56bf76889780ce50b866e9ed4e621e9ea8895
ODMich <97339732+ODMich@users.noreply.github.com>
2025-10-06 15:40:42 +0100

    more

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,7 +11,9 @@ command -v kubectl >/dev/null 2>&1 || { echo "Kubectl required but not installed
 # Start Minikube
 if ! minikube status | grep -q "Running"; then
     echo "Starting Minikube cluster..."
-    minikube start --addons=ingress --cpus=4 --memory=8192
+    minikube start --addons=ingress --cpus=4 --memory=6500 --disk-size=4g
+    minikube addons enable ingress
+    
 else
     echo "Minikube is already running"
     minikube addons enable ingress
